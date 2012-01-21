b16148e0cea8e269ae2073b736600a9e646d0a39
linduxed <linduxed@gmail.com>
2012-01-21 13:41:52 +0100

    Updated the vim plugins and made some changes.
    A verification option for ZSH was disabled.
    Auto-Pairs had FastWrap disabled and had the script toggling remapped.

--- .oh-my-zsh/lib/history.zsh
+++ .oh-my-zsh/lib/history.zsh
@@ -9,6 +9,5 @@ setopt hist_expire_dups_first
 setopt hist_ignore_dups # ignore duplication command history list
 setopt hist_save_no_dups
 setopt hist_ignore_space
-setopt hist_verify
 setopt inc_append_history
 setopt share_history # share command history data
