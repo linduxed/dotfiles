e9a529597371a66e0b56f651bf59bf9b77112873
Mikael Muszynski <linduxed@gmail.com>
2022-06-29 09:45:18 +0200

    Neovim: change plugin_settings.lua formatting
    To make it easier to add Lua configuration for plugins, have each plugin
    configuration be surrounded with the `vim.cmd` invocation. It's extra
    noise, but having the organizational comments with Lua syntax and the
    Vimscript configuration wrapped results in easier addition/deletion for
    configuration.

--- config/nvim/lua/plugin_settings.lua
+++ config/nvim/lua/plugin_settings.lua
@@ -1,10 +1,12 @@
+-- {{{1 abolish
+
 vim.cmd([[
-" {{{1 abolish
-
 let g:abolish_save_file = "/home/linduxed/.config/nvim/after/plugin/abolish.vim"
+]])
 
-" {{{1 airline
+-- {{{1 airline
 
+vim.cmd([[
 let g:airline_powerline_fonts = 1
 let g:airline#extensions#branch#enabled = 1
 let g:airline#extensions#tagbar#enabled = 1
@@ -13,14 +15,18 @@ let g:airline#extensions#hunks#non_zero_only = 1
 let g:airline#extensions#branch#displayed_head_limit = 12
 let g:airline#extensions#capslock#enabled = 1
 let g:airline_section_z = airline#section#create(['%{ObsessionStatus(''◆'', '''')}', 'windowswap', '%3p%% ', 'linenr', ':%3v '])
+]])
 
-" {{{1 bullets
+-- {{{1 bullets
 
+vim.cmd([[
 let g:bullets_outline_levels = ['num']
 let g:bullets_set_mappings = 0
+]])
 
-" {{{1 DelimitMate
+-- {{{1 DelimitMate
 
+vim.cmd([[
 " This is the default, but is included here for clarity:
 " The coc.vim completion menu needs a <CR> mapping to work reliably.
 " Therefore this should not be set to 1.
@@ -28,9 +34,11 @@ let delimitMate_expand_cr = 0
 
 let delimitMate_expand_space = 1
 let delimitMate_jump_expansion = 1
+]])
 
-" {{{1 EasyMotion
+-- {{{1 EasyMotion
 
+vim.cmd([[
 hi link EasyMotionTarget ErrorMsg
 hi link EasyMotionShade Comment
 hi link EasyMotionTarget2First Identifier
@@ -38,36 +46,48 @@ hi link EasyMotionTarget2Second Identifier
 
 " Characters at the end of the string have higher precedence.
 let g:EasyMotion_keys = 'hdoairesnt'
+]])
 
-" {{{1 ferret
+-- {{{1 ferret
 
+vim.cmd([[
 let g:FerretMap = 0
 let g:FerretQFCommands = 0
+]])
 
-" {{{1 fugitive
+-- {{{1 fugitive
 
+vim.cmd([[
 let g:fugitive_no_maps = 1
+]])
 
-" {{{1 fzf
+-- {{{1 fzf
 
+vim.cmd([[
 command! -bang -nargs=* Rg
   \ call fzf#vim#grep(
   \   'rg --column --line-number --no-heading --color=always --smart-case '.shellescape(<q-args>), 1,
   \   fzf#vim#with_preview(), <bang>0)
+]])
 
-" {{{1 Gist
+-- {{{1 Gist
 
+vim.cmd([[
 let g:gist_clip_command = 'xclip -selection clipboard'
 let g:gist_detect_filetype = 1
 let g:gist_open_browser_after_post = 1
 let g:gist_browser_command = 'xdg-open %URL%'
+]])
 
-" {{{1 gitgutter
+-- {{{1 gitgutter
 
+vim.cmd([[
 let g:gitgutter_map_keys = 0
+]])
 
-" {{{1 Goyo
+-- {{{1 Goyo
 
+vim.cmd([[
 let g:goyo_bg = 232
 
 function! s:goyo_enter()
@@ -82,15 +102,19 @@ endfunction
 
 autocmd! User GoyoEnter nested call <SID>goyo_enter()
 autocmd! User GoyoLeave nested call <SID>goyo_leave()
+]])
 
-" {{{1 grepper
+-- {{{1 grepper
 
+vim.cmd([[
 let g:grepper = {}
 let g:grepper.tools = ['rg', 'git', 'ag']
 let g:grepper.highlight = 1
+]])
 
-" {{{1 Mark
+-- {{{1 Mark
 
+vim.cmd([[
 let g:mwDefaultHighlightingPalette = [
 \   { 'ctermbg':'Cyan',    'ctermfg':'Black' },
 \   { 'ctermbg':'Green',   'ctermfg':'Black' },
@@ -147,9 +171,11 @@ let g:mwDefaultHighlightingPalette = [
 \   { 'ctermfg':'Black',   'ctermbg':'224' },
 \   { 'ctermfg':'Black',   'ctermbg':'228' },
 \]
+]])
 
-" {{{1 Neomake
+-- {{{1 Neomake
 
+vim.cmd([[
 let g:neomake_ruby_enabled_makers = ['mri', 'rubocop']
 let g:neomake_elixir_enabled_makers = ['credo', 'dogma']
 let g:neomake_warning_sign = {
@@ -160,9 +186,11 @@ let g:neomake_error_sign = {
   \ 'texthl': 'Error',
   \ }
 autocmd! BufWritePost * Neomake
+]])
 
-" {{{1 NERDcommenter
+-- {{{1 NERDcommenter
 
+vim.cmd([[
 let NERDSpaceDelims = 1 " Add spaces around the comment signs for all languages
 let NERDDefaultAlign = 'left' " Placement of aligned comment symbols
 let NERDCommentEmptyLines = 1 " Empty lines will be commented as well
@@ -175,13 +203,17 @@ let g:NERDCustomDelimiters = {
     \ 'hspec': { 'leftAlt': '{-','rightAlt': '-}', 'left': '--', 'right': '' },
     \ 'c': { 'leftAlt': '/*','rightAlt': '*/', 'left': '//', 'right': '' },
 \ }
+]])
 
-" {{{1 NERDtree
+-- {{{1 NERDtree
 
+vim.cmd([[
 let NERDTreeQuitOnOpen = 1
+]])
 
-" {{{1 tagbar
+-- {{{1 tagbar
 
+vim.cmd([[
 let g:tagbar_type_elixir = {
     \ 'ctagstype' : 'elixir',
     \ 'kinds' : [
@@ -199,30 +231,38 @@ let g:tagbar_type_elixir = {
         \ 't:tests'
     \ ]
 \ }
+]])
 
-" {{{1 taskwiki
+-- {{{1 taskwiki
 
+vim.cmd([[
 let g:taskwiki_sort_orders={"E": "end-"}
+]])
 
-" {{{1 TextObjects
+-- {{{1 TextObjects
 
+vim.cmd([[
 " Default bindings disabled for remapping to Colemak bindings.
 let g:textobj_rubyblock_no_default_key_mappings = 1
 let g:textobj_indent_no_default_key_mappings = 1
 let g:textobj_space_no_default_key_mappings = 1
 let g:textobj_underscore_no_default_key_mappings = 1
 let g:textobj_url_no_default_key_mappings = 1
+]])
 
-" {{{1 unimpaired
+-- {{{1 unimpaired
 
+vim.cmd([[
 " The default mapping prefix 'yo' conflicts with my Colemak setup.
 " The older prefix 'co' works better for me.
 let g:nremap = {"y": "c"}
 let g:xremap = {"y": "c"}
 let g:oremap = {"y": "c"}
+]])
 
-" {{{1 vimwiki
+-- {{{1 vimwiki
 
+vim.cmd([[
 let g:vimwiki_auto_chdir = 1
 let g:vimwiki_list = [
   \{
@@ -243,18 +283,24 @@ let g:vimwiki_list = [
 
 " add the <pre> tag, for inserting code snippets
 let g:vimwiki_valid_html_tags = 'b,i,s,u,sub,sup,kbd,br,hr,pre,script'
+]])
 
-" {{{1 vista
+-- {{{1 vista
 
+vim.cmd([[
 let g:vista_icon_indent = ["↳ ", "↦ "]
+]])
 
-" {{{1 wheel
+-- {{{1 wheel
 
+vim.cmd([[
 " Disable for mouse, due to erratic behavior.
 let g:wheel#map#mouse = 0
+]])
 
-" {{{1 zettel
+-- {{{1 zettel
 
+vim.cmd([[
 let g:zettel_format = "z_%Y-%m-%d_%H%M"
 let g:zettel_fzf_command = "rg --column --line-number --ignore-case --no-heading --color=always "
 ]])
