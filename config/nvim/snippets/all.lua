269190dfc18b1ccc2cfec6c912553a267489aac3
Mikael Muszynski <linduxed@gmail.com>
2022-08-08 15:33:01 +0200

    Neovim: add hex tag snippets
    Occasionally I have the need of a tag comprised of hex characters, e.g.
    when I rename files with `vidir` and want to avoid file name collisions.
    I used to use the `uuid` snippet from `vim-snippets`, since I no longer
    import all of those snippets, I decided to reimplement a minimal version
    of the snippet for my use case.
    
    I find it likely that the implementation I chose is not safe to use for
    very large amounts of objects, but my for my very limited amount of
    files that I would apply this to, it's more than enough.
    
    This patch adds two versions of the tag generation (6 and 8 chars),
    mostly due to legacy reasons: some places us 6 character random strings,
    others use 8 character strings.

--- config/nvim/snippets/all.lua
+++ config/nvim/snippets/all.lua
@@ -1,3 +1,18 @@
+local p = require("luasnip.extras").partial
+local random_hex = function(target_string_length)
+    local hex = {
+        "0", "1", "2", "3", "4", "5", "6", "7", "8", "9",
+        "a", "b", "c", "d", "e", "f",
+    }
+
+    local chosen_chars = {}
+    for i = 1, target_string_length, 1 do
+        chosen_chars[i] = hex[math.random(#hex)]
+    end
+
+    return table.concat(chosen_chars)
+end
+
 return {
     s(
         {
@@ -15,4 +30,22 @@ return {
         },
         { p(os.date, "%Y-%m-%d"), }
     ),
+    s(
+        {
+            trig = "hex6tag",
+            name = "Random 6 hex characters tag",
+            dscr = "Tag comprised of random hex characters, for simple " ..
+                "creation of a semi-unique tag."
+        },
+        p(random_hex, 6)
+    ),
+    s(
+        {
+            trig = "hex8tag",
+            name = "Random 8 hex characters tag",
+            dscr = "Tag comprised of random hex characters, for simple " ..
+                "creation of a semi-unique tag."
+        },
+        p(random_hex, 8)
+    )
 }
