d33c97ec8f914adcef99ab4e0de62a1ca61c045f
Mikael Muszynski <linduxed@gmail.com>
2022-08-09 17:49:40 +0200

    Neovim: add more snippets

--- config/nvim/snippets/elixir.lua
+++ config/nvim/snippets/elixir.lua
@@ -1,3 +1,25 @@
+local s = require("luasnip.nodes.snippet").S
+local sn = require("luasnip.nodes.snippet").SN
+local t = require("luasnip.nodes.textNode").T
+-- local f = require("luasnip.nodes.functionNode").F
+local i = require("luasnip.nodes.insertNode").I
+local c = require("luasnip.nodes.choiceNode").C
+-- local d = require("luasnip.nodes.dynamicNode").D
+-- local r = require("luasnip.nodes.restoreNode").R
+-- local l = require("luasnip.extras").lambda
+-- local rep = require("luasnip.extras").rep
+-- local p = require("luasnip.extras").partial
+-- local m = require("luasnip.extras").match
+-- local n = require("luasnip.extras").nonempty
+-- local dl = require("luasnip.extras").dynamic_lambda
+-- local fmt = require("luasnip.extras.fmt").fmt
+-- local fmta = require("luasnip.extras.fmt").fmta
+-- local conds = require("luasnip.extras.expand_conditions")
+-- local types = require("luasnip.util.types")
+-- local events = require("luasnip.util.events")
+-- local parse = require("luasnip.util.parser").parse_snippet
+-- local ai = require("luasnip.nodes.absolute_indexer")
+
 return {
     s(
         { trig = "p", name = "pipe" },
@@ -6,6 +28,14 @@ return {
     s(
         { trig = "fn", name = "lambda function" },
         { t("fn "), i(1, "x"), t(" -> "), i(2, ""), t(" end") }
+    ),
+    s(
+        { trig = "fnm", name = "lambda function (multiline)" },
+        {
+            t("fn "), i(1, "x"), t({ " ->",
+                "  " }), i(2, ""), t({ "",
+                "end" }),
+        }
     ),
     s(
         { trig = "amp", name = "function capture ampersand" },
@@ -29,6 +59,10 @@ return {
     s(
         { trig = "penum", name = "pipe-enum" },
         { t("|> Enum."), i(1, "") }
+    ),
+    s(
+        { trig = "enum", name = "bare-enum" },
+        { t("Enum."), i(1, "") }
     ),
     s(
         { trig = "pstr", name = "pipe-stream" },
