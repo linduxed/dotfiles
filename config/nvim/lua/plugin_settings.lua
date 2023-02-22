6342487112e330d547a396fdd8797a9c06097747
Mikael Muszynski <linduxed@protonmail.com>
2023-02-22 15:29:51 +0100

    Neovim: disable vim-markdown key mappings
    I haven't figured out which mappings I want to use from this plugin, and
    I certainly don't want to get surprised suddenly when editing markdown
    documents by suddenly stumbling over a mapping I didn't expect.

--- config/nvim/lua/plugin_settings.lua
+++ config/nvim/lua/plugin_settings.lua
@@ -182,6 +182,7 @@ ls.config.set_config({
 vim.g.vim_markdown_folding_disabled = 1
 vim.g.vim_markdown_auto_insert_bullets = 0
 vim.g.vim_markdown_new_list_item_indent = 0
+vim.g.vim_markdown_no_default_key_mappings = 1
 
 -- {{{1 Mark
 
