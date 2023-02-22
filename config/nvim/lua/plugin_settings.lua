b0bb746204ce8a0682b25c45945723ce84c7b21a
Mikael Muszynski <linduxed@protonmail.com>
2023-02-22 15:31:01 +0100

    Neovim: disable vim-markdown concealing of code fences
    While the concealing features of vim-markdown are pretty nice, I don't
    feel like having the code fences disappear is helpful. I might change
    opinion on this in the future, but for now, I want them visible.

--- config/nvim/lua/plugin_settings.lua
+++ config/nvim/lua/plugin_settings.lua
@@ -183,6 +183,7 @@ vim.g.vim_markdown_folding_disabled = 1
 vim.g.vim_markdown_auto_insert_bullets = 0
 vim.g.vim_markdown_new_list_item_indent = 0
 vim.g.vim_markdown_no_default_key_mappings = 1
+vim.g.vim_markdown_conceal_code_blocks = 0
 
 -- {{{1 Mark
 
