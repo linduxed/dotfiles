3b4bd39f17e187d50cad4e89caf1155024d1088a
Mikael Muszynski <linduxed@protonmail.com>
2023-04-25 22:28:44 +0200

    Neovim: replace vim-gitgutter with gitsigns.nvim
    Beyond the fact that the `gitsigns.nvim` plugin seems to be more
    featureful than `vim-gitgutter`, it's also written in Lua and therefore
    I presume it'll work better with Neovim and the other plugins from the
    ecosystem.

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -436,21 +436,48 @@ local lazy_setup = {
         end
     },
     {
-        "airblade/vim-gitgutter",
-        init = function()
-            vim.g.gitgutter_map_keys = 0
-        end,
-        keys = {
-            { "]c",           "<plug>(gitgutternexthunk)" },
-            { "[c",           "<plug>(gitgutterprevhunk)" },
-            { "<leader>agtb", ":gitgutterlinehighlightstoggle<cr>" },
-            { "<leader>agtn", ":gitgutterlinenrhighlightstoggle<cr>" },
-            { "<leader>agts", ":gitguttersignstoggle<cr>" },
-            { "<leader>agf",  ":gitgutterfold<cr>" },
-            { "<leader>ags",  ":gitgutterstagehunk<cr>" },
-            { "<leader>agu",  ":gitgutterundohunk<cr>" },
-            { "<leader>agp",  ":gitgutterpreviewhunk<cr>" },
-        }
+        'lewis6991/gitsigns.nvim',
+        config = function()
+            require('gitsigns').setup {
+                signcolumn = true,          -- Toggle with `:Gitsigns toggle_signs`
+                numhl = false,              -- Toggle with `:Gitsigns toggle_numhl`
+                linehl = false,             -- Toggle with `:Gitsigns toggle_linehl`
+                word_diff = false,          -- Toggle with `:Gitsigns toggle_word_diff`
+                current_line_blame = false, -- Toggle with `:Gitsigns toggle_current_line_blame`
+                current_line_blame_opts = {
+                    virt_text = true,
+                    virt_text_pos = 'eol', -- 'eol' | 'overlay' | 'right_align'
+                    delay = 1000,
+                    ignore_whitespace = false,
+                },
+                current_line_blame_formatter = ' <author>, <author_time:%Y-%m-%d> - <summary>',
+                on_attach = function(bufnr)
+                    local gs = package.loaded.gitsigns
+
+                    local function map(mode, l, r, opts)
+                        opts = opts or {}
+                        opts.buffer = bufnr
+                        vim.keymap.set(mode, l, r, opts)
+                    end
+
+                    -- Navigation
+                    map('n', ']c', function()
+                        if vim.wo.diff then return ']c' end
+                        vim.schedule(function() gs.next_hunk() end)
+                        return '<Ignore>'
+                    end, { expr = true })
+
+                    map('n', '[c', function()
+                        if vim.wo.diff then return '[c' end
+                        vim.schedule(function() gs.prev_hunk() end)
+                        return '<Ignore>'
+                    end, { expr = true })
+
+                    -- Text object
+                    map({ 'o', 'x' }, 'rh', ':<C-U>Gitsigns select_hunk<CR>')
+                end
+            }
+        end
     },
     {
         "plasticboy/vim-markdown",
