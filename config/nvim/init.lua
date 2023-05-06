80140961850124a0206d862ef13230fc0b6f7086
Mikael Muszynski <linduxed@protonmail.com>
2023-05-07 01:47:22 +0200

    Neovim: add edit-markdown-table plugin
    Occasionally I edit my Obsidian vault files in Neovim, and for those
    cases where I have a markdown table, this plugin could be useful.
    Might not use it much, but I'll include it, for now.

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -882,6 +882,14 @@ local lazy_setup = {
             })
         end
     },
+    {
+        'kiran94/edit-markdown-table.nvim',
+        config = function()
+            require('edit-markdown-table').setup()
+        end,
+        dependencies = { "nvim-treesitter/nvim-treesitter" },
+        cmd = "EditMarkdownTable",
+    },
 
     -- Text objects
     {
