f8b5e0b9f2dc7a43f9a1cc09a1ab55a58893bc6b
Mikael Muszynski <linduxed@protonmail.com>
2023-06-14 07:15:56 +0200

    Neovim: replace Goyo with ZenMode
    Goyo is having a lot of extra artifacts visible with my current Neovim
    configuration, partially defeating the purpose. I therefore wanted to
    find a replacement made for Neovim, and `zen-mode.nvim` seems to be
    working very well.

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -151,30 +151,43 @@ local lazy_setup = {
     },
     "Rawnly/gist.nvim",
     {
-        "junegunn/goyo.vim",
-        init = function()
-            vim.g.goyo_bg = 232
-        end,
-        config = function()
-            -- This does not seem to work, as cursorline and cursorcolumn are
-            -- still visible.
-            vim.cmd([[
-            function! s:goyo_enter()
-            set nocursorcolumn
-            set nocursorline
-            endfunction
-
-            function! s:goyo_leave()
-            set cursorcolumn
-            set cursorline
-            endfunction
-
-            autocmd! user goyoenter nested call <sid>goyo_enter()
-            autocmd! user goyoleave nested call <sid>goyo_leave()
-            ]])
-        end,
+        "folke/zen-mode.nvim",
+        opts = {
+            window = {
+                backdrop = 0.95, -- shade the backdrop of the Zen window. Set to 1 to keep the same as Normal
+                -- height and width can be:
+                -- * an absolute number of cells when > 1
+                -- * a percentage of the width / height of the editor when <= 1
+                -- * a function that returns the width or the height
+                width = 100, -- width of the Zen window
+                height = 0.95, -- height of the Zen window
+                -- by default, no options are changed for the Zen window
+                -- uncomment any of the options below, or add other vim.wo options you want to apply
+                options = {
+                    signcolumn = "no", -- disable signcolumn
+                    number = false, -- disable number column
+                    relativenumber = false, -- disable relative numbers
+                    cursorline = false, -- disable cursorline
+                    cursorcolumn = false, -- disable cursor column
+                    foldcolumn = "0", -- disable fold column
+                    list = false, -- disable whitespace characters
+                },
+            },
+            plugins = {
+                -- disable some global vim options (vim.o...)
+                -- comment the lines to not apply the options
+                options = {
+                    enabled = true,
+                    ruler = false,  -- disables the ruler text in the cmd line area
+                    showcmd = false, -- disables the command in the last line of the screen
+                },
+                twilight = { enabled = true }, -- enable to start Twilight when zen mode opens
+                gitsigns = { enabled = false }, -- disables git signs
+                tmux = { enabled = false }, -- disables the tmux statusline
+            },
+        },
         keys = {
-            { "<Leader>go", ":Goyo<CR>" }
+            { "<Leader>go", "<cmd>ZenMode<CR>" }
         }
     },
     {
