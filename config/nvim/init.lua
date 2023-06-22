821855d5454de9809b8fef1e1d663de38856cc5f
Mikael Muszynski <linduxed@protonmail.com>
2023-06-22 16:29:05 +0200

    Neovim: change size of Zen window to be smaller
    100 felt a bit too big.

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -159,7 +159,7 @@ local lazy_setup = {
                 -- * an absolute number of cells when > 1
                 -- * a percentage of the width / height of the editor when <= 1
                 -- * a function that returns the width or the height
-                width = 100, -- width of the Zen window
+                width = 80, -- width of the Zen window
                 height = 0.95, -- height of the Zen window
                 -- by default, no options are changed for the Zen window
                 -- uncomment any of the options below, or add other vim.wo options you want to apply
