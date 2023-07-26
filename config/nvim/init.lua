811a90d6650e1be6ae8c6da896d01b87d8894eb9
Mikael Muszynski <linduxed@protonmail.com>
2023-07-27 01:18:46 +0200

    Neovim: remove example comments

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -1324,20 +1324,10 @@ local lazy_setup = {
             })
             local wk = require("which-key")
 
-            -- As an example, we will create the following mappings:
-            --  * <leader>ff find files
-            --  * <leader>fr show recent files
-            --  * <leader>fb Foobar
-            -- we'll document:
-            --  * <leader>fn new file
-            --  * <leader>fe edit file
-            -- and hide <leader>1
-
             wk.register({
                 ["<leader>"] = {
                     a = {
                         name = "Misc #1",
-                        -- create a binding with label
                     },
             }, {})
         end
