ab14214b616f794220cd32dbd430594e4c95e0b6
Mikael Muszynski <linduxed@protonmail.com>
2023-07-27 01:21:50 +0200

    Neovim: apply style formatter

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -508,17 +508,17 @@ local lazy_setup = {
             -- Misc. mappings
             vim.keymap.set("n",
                 "<C-p>",
-                function () require('telescope.builtin').find_files({follow = true}) end,
+                function() require('telescope.builtin').find_files({ follow = true }) end,
                 { desc = "Telescope - Files" }
             )
             vim.keymap.set("n",
                 "<leader>fg",
-                function () require('telescope.builtin').live_grep() end,
+                function() require('telescope.builtin').live_grep() end,
                 { desc = "Telescope - Line, all files, exact grep" }
             )
             vim.keymap.set("n",
                 "<leader>fr",
-                function ()
+                function()
                     require('telescope.builtin').grep_string({
                         shorten_path = true,
                         word_match = "-w",
@@ -530,27 +530,27 @@ local lazy_setup = {
             )
             vim.keymap.set("n",
                 "<leader>fh",
-                function () require('telescope.builtin').help_tags() end,
+                function() require('telescope.builtin').help_tags() end,
                 { desc = "Telescope - Help tags" }
             )
             vim.keymap.set("n",
                 "<leader>fm",
-                function () require('telescope.builtin').marks() end,
+                function() require('telescope.builtin').marks() end,
                 { desc = "Telescope - Marks" }
             )
             vim.keymap.set("n",
                 "<leader>fb",
-                function () require('telescope.builtin').current_buffer_fuzzy_find() end,
+                function() require('telescope.builtin').current_buffer_fuzzy_find() end,
                 { desc = "Telescope - Line, current buffer, fuzzy" }
             )
             vim.keymap.set("n",
                 "<leader>ft",
-                function () require('telescope.builtin').treesitter() end,
+                function() require('telescope.builtin').treesitter() end,
                 { desc = "Telescope - Treesitter" }
             )
             vim.keymap.set("n",
                 "<leader>fap",
-                function () require('telescope.builtin').builtin() end,
+                function() require('telescope.builtin').builtin() end,
                 { desc = "Telescope - List all pickers" }
             )
             vim.keymap.set("n",
@@ -569,12 +569,12 @@ local lazy_setup = {
             -- LSP mappings
             vim.keymap.set("n",
                 "<leader>alr",
-                function () require('telescope.builtin').lsp_references() end,
+                function() require('telescope.builtin').lsp_references() end,
                 { desc = "Telescope - LSP - references" }
             )
             vim.keymap.set("n",
                 "<leader>ali",
-                function () require('telescope.builtin').lsp_implementations() end,
+                function() require('telescope.builtin').lsp_implementations() end,
                 { desc = "Telescope - LSP - Implementations" }
             )
         end,
