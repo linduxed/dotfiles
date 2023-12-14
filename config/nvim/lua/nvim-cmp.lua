b49e7a73a9221019b192e1e3f710af2a8380e1b7
Mikael Muszynski <linduxed@protonmail.com>
2023-12-14 14:59:05 +0100

    Neovim: make the nvim-cmp "buffer" source complete from visible buffers
    Currently the "buffer" source (plugin name: `cmp-buffer`) is only
    completing words from the current buffer. I remember that I used to have
    completion from all visible buffers, back when I was using coc.nvim,
    i.e. _much_ more useful.
    
    This should be the default, I think, but anyway, this fixes it.

--- config/nvim/lua/nvim-cmp.lua
+++ config/nvim/lua/nvim-cmp.lua
@@ -17,6 +17,13 @@ local buffer_source = {
     name = 'buffer',
     option = {
         keyword_pattern = [[\k\+]],
+        get_bufnrs = function()
+            local bufs = {}
+            for _, win in ipairs(vim.api.nvim_list_wins()) do
+                bufs[vim.api.nvim_win_get_buf(win)] = true
+            end
+            return vim.tbl_keys(bufs)
+        end
     }
 }
 
