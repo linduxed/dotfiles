beee0f1eca0652008a98b49c9533d518f585a017
Mikael Muszynski <linduxed@protonmail.com>
2023-12-14 13:02:53 +0100

    Neovim: remove Tagbar
    Since I've transitioned to using Vista (for its integration with
    Neovim's Treesitter functionality), I have no use for Tagbar. Treesitter
    covers the features I get from ctags, so it seems redundant to both have
    Vista and Tagbar.

--- config/nvim/lua/plugins/misc.lua
+++ config/nvim/lua/plugins/misc.lua
@@ -401,38 +401,6 @@ return {
         }
     },
     {
-        "majutsushi/tagbar",
-        init = function()
-            vim.g.tagbar_type_elixir = {
-                ctagstype = "elixir",
-                kinds = {
-                    "p:protocols",
-                    "m:modules",
-                    "e:exceptions",
-                    "y:types",
-                    "d:delegates",
-                    "f:functions",
-                    "c:callbacks",
-                    "a:macros",
-                    "t:tests",
-                    "i:implementations",
-                    "o:operators",
-                    "r:records"
-                },
-                sro = ".",
-                kind2scope = {
-                    p = "protocol",
-                    m = "module"
-                },
-                scope2kind = {
-                    protocol = "p",
-                    module = "m"
-                },
-                sort = 0
-            }
-        end,
-    },
-    {
         "folke/trouble.nvim",
         config = function()
             require("trouble").setup({})
