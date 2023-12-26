06f0cdb05deab41ceaf41da1fa461aabf6401cfb
Mikael Muszynski <linduxed@protonmail.com>
2023-12-26 10:44:15 +0100

    Neovim: don't load copilot.lua on the first Insert
    This came from a copy-paste of the configuration for packer, converted
    to lazy.nvim configuration. Maybe packer works differently, where one
    needs to run the `cmd` before packer cares about `event`, but with
    lazy.nvim, going into Insert mode is sufficient for loading Copilot,
    which is not what I want.

--- config/nvim/lua/plugins/misc.lua
+++ config/nvim/lua/plugins/misc.lua
@@ -889,7 +889,6 @@ return {
     {
         "zbirenbaum/copilot.lua",
         cmd = "Copilot",
-        event = "InsertEnter",
         config = function()
             require("copilot").setup({})
         end,
