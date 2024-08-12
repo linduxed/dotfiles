b4e1d8c8be0db267ae9e73eb14265b66198acc80
Mikael Muszynski <linduxed@protonmail.com>
2024-08-12 13:04:42 +0200

    Fish: extend "notes" with functionality from old Zsh config
    At some point I'll have to figure out how to best refactor `notes`, to
    DRY it up somewhat, but for now this will do.

--- config/fish/functions/notes.fish
+++ config/fish/functions/notes.fish
@@ -1,9 +1,11 @@
 function notes --description 'Manage notes repository'
     function __print_help --description "Print 'notes' help."
         printf "Usage: notes [-smh]\n\n"
-        printf "         -s --sync     Stage all files, commit, pull-rebase latest, push\n"
-        printf "         -m --cd       `cd` to the notes directory\n"
-        printf "         -h --help     Print this help\n"
+        printf "         -s --sync              Stage all files, commit, pull-rebase latest, push\n"
+        printf "         -a --commit-all        Stage all files and commit,\n"
+        printf "         -c --commit-staged     Commit all staged files\n"
+        printf "         -m --cd                `cd` to the notes directory\n"
+        printf "         -h --help              Print this help\n"
     end
 
     function __notes_sync --description "Stage all files, commit, pull-rebase latest, push"
@@ -15,13 +17,26 @@ function notes --description 'Manage notes repository'
         git -C $notes_dir push
     end
 
+    function __notes_commit_all --description "Stage all files and commit"
+        set -l notes_dir "$HOME/vimwiki"
+
+        git -C $notes_dir add . &&
+        git -C $notes_dir commit -m "$(hostname): $(date -Iseconds)"
+    end
+
+    function __notes_commit_staged --description "Commit all staged files"
+        set -l notes_dir "$HOME/vimwiki"
+
+        git -C $notes_dir commit -m "$(hostname): $(date -Iseconds)"
+    end
+
     function __notes_cd --description "`cd` to the notes directory"
         set -l notes_dir "$HOME/vimwiki"
 
         cd $notes_dir
     end
 
-    set -l options s/sync m/cd h/help
+    set -l options s/sync a/commit-all c/commit-staged m/cd h/help
 
     argparse $options -- $argv
 
@@ -33,6 +48,12 @@ function notes --description 'Manage notes repository'
         return 0
     else if set -q _flag_sync
         __notes_sync
+        return 0
+    else if set -q _flag_commit_all
+        __notes_commit_all
+        return 0
+    else if set -q _flag_commit_staged
+        __notes_commit_staged
         return 0
     end
 end
