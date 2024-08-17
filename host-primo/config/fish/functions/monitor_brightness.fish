e0b3e5bf2a801e3cb74424830de5e563c1b030de
Mikael Muszynski <linduxed@protonmail.com>
2024-08-17 16:35:50 +0200

    fish: add monitor brightness adjustment function for primo host
    Porting the function that I had already implemented for Zsh.

---
+++ host-primo/config/fish/functions/monitor_brightness.fish
@@ -0,0 +1,4 @@
+function monitor_brightness --description "Changes brightness with ddcutil on monitors 1 & 2"
+    sudo ddcutil -d 1 setvcp 10 $argv[1]
+    sudo ddcutil -d 2 setvcp 10 $argv[1]
+end
