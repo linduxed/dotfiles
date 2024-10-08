591b67b4bec9e9148633da5a2ce9d1c4b3540327
Mikael Muszynski <linduxed@protonmail.com>
2024-10-08 07:31:40 +0200

    feat(Neovim): configure outline.nvim more and add another mapping

--- config/nvim/lua/plugins/misc.lua
+++ config/nvim/lua/plugins/misc.lua
@@ -904,12 +904,75 @@ return {
         cmd = { "Outline", "OutlineOpen" },
         keys = {
             {
+                "<F9>",
+                "<cmd>OutlineFocus<CR>",
+                desc = "Focus outline",
+            },
+            {
                 "<F10>",
                 "<cmd>Outline<CR>",
                 desc = "Toggle outline",
             },
         },
-        opts = {},
+        opts = {
+            outline_window = {
+                width = 60,
+                relative_width = false,
+                focus_on_open = false,
+            },
+
+            preview_window = {
+                width = 50,     -- Percentage or integer of columns
+                min_width = 50, -- Minimum number of columns
+                relative_width = true,
+                height = 50,     -- Percentage or integer of lines
+                min_height = 10, -- Minimum number of lines
+                relative_height = true,
+                border = 'double',
+                live = true
+            },
+
+            -- These keymaps can be a string or a table for multiple keys.
+            -- Set to `{}` to disable. (Using 'nil' will fallback to default keys)
+            keymaps = {
+                show_help = '?',
+                close = { '<Esc>', 'q' },
+                -- Jump to symbol under cursor.
+                -- It can auto close the outline window when triggered, see
+                -- 'auto_close' option above.
+                goto_location = '<CR>',
+                -- Jump to symbol under cursor but keep focus on outline window.
+                peek_location = 'o',
+                -- Visit location in code and close outline immediately
+                goto_and_close = '<S-CR>',
+                -- Change cursor position of outline window to match current location in code.
+                -- 'Opposite' of goto/peek_location.
+                restore_location = '<C-g>',
+                -- Open LSP/provider-dependent symbol hover information
+                hover_symbol = '<C-space>',
+                -- Preview location code of the symbol under cursor
+                toggle_preview = 'M',
+                rename_symbol = 'r',
+                code_actions = 'a',
+                -- These fold actions are collapsing tree nodes, not code folding
+                fold = 'h',
+                unfold = 'l',
+                fold_toggle = '<Tab>',
+                -- Toggle folds for all nodes.
+                -- If at least one node is folded, this action will fold all nodes.
+                -- If all nodes are folded, this action will unfold all nodes.
+                fold_toggle_all = '<S-Tab>',
+                fold_all = 'W',
+                unfold_all = 'E',
+                fold_reset = 'R',
+                -- Move down/up by one line and peek_location immediately.
+                -- You can also use outline_window.auto_jump=true to do this for any
+                -- j/k/<down>/<up>.
+                down_and_jump = '<C-j>',
+                up_and_jump = '<C-k>',
+            },
+
+        },
     },
     {
         "reedes/vim-wheel",
