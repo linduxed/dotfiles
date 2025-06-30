cfa3596a4e0feb1ee703e8a1e8ef1fbe2968c3d1
Mikael Muszynski <linduxed@protonmail.com>
2025-06-30 10:06:40 +0200

    feat(neovim)!: remove null-ls, due to deprecation and breakage
    This plugin has been deprecated for almost 2 years now, and today I ran
    into it breaking all the time when I opened a JSON file. I think it's
    time to remove it, since I don't even really need the things I have it
    configured for.

--- config/nvim/lua/plugins/lsp.lua
+++ config/nvim/lua/plugins/lsp.lua
@@ -1,25 +1,6 @@
 return {
     "neovim/nvim-lspconfig",
     {
-        "jose-elias-alvarez/null-ls.nvim",
-        dependencies = {
-            "nvim-lua/plenary.nvim",
-        },
-        config = function()
-            local null_ls = require("null-ls")
-
-            null_ls.setup({
-                sources = {
-                    -- null_ls.builtins.diagnostics.credo,
-                    null_ls.builtins.diagnostics.zsh,
-                    -- null_ls.builtins.formatting.erlfmt,
-                    null_ls.builtins.formatting.fnlfmt,
-                    null_ls.builtins.formatting.jq,
-                }
-            })
-        end
-    },
-    {
         "luckasRanarison/tailwind-tools.nvim",
         name = "tailwind-tools",
         build = ":UpdateRemotePlugins",
