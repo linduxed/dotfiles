afaa3f59bd2b4b1d4fb2cc5fce7ba42f717c6162
Mikael Muszynski <linduxed@protonmail.com>
2025-07-10 04:54:31 +0200

    fix(neovim)!: set nvim-surround mappings to use `z` instead of `s`
    I've noticed that flash.nvim uses the `s` key in Operator-mode, which
    nvim-surround does as well. I suspect that I'll find flash.nvim more
    freqently used, and therefore I'll move surrounding to the `z` key, with
    the mnemonic of "zurround".

--- config/nvim/lua/plugins/misc.lua
+++ config/nvim/lua/plugins/misc.lua
@@ -609,6 +609,19 @@ return {
         event = "VeryLazy",
         config = function()
             require("nvim-surround").setup({
+                keymaps = {
+                    insert = "<C-g>z",
+                    insert_line = "<C-g>Z",
+                    normal = "yz",
+                    normal_cur = "yzz",
+                    normal_line = "yZ",
+                    normal_cur_line = "yZZ",
+                    visual = "Z",
+                    visual_line = "gZ",
+                    delete = "dz",
+                    change = "cz",
+                    change_line = "cZ",
+                },
                 aliases = {
                     ["a"] = ">",
                     ["b"] = ")",
