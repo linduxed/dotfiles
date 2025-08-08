29aa5b91966db5871069a1ec0401456a6534ab39
Mikael Muszynski <linduxed@protonmail.com>
2025-08-08 12:45:45 +0200

    style(neovim): do not use trailing comments with alignment

--- config/nvim/lua/plugins/version_control.lua
+++ config/nvim/lua/plugins/version_control.lua
@@ -12,11 +12,16 @@ return {
         'lewis6991/gitsigns.nvim',
         config = function()
             require('gitsigns').setup {
-                signcolumn = true,          -- Toggle with `:Gitsigns toggle_signs`
-                numhl = false,              -- Toggle with `:Gitsigns toggle_numhl`
-                linehl = false,             -- Toggle with `:Gitsigns toggle_linehl`
-                word_diff = false,          -- Toggle with `:Gitsigns toggle_word_diff`
-                current_line_blame = false, -- Toggle with `:Gitsigns toggle_current_line_blame`
+                -- Toggle with `:Gitsigns toggle_signs`
+                signcolumn = true,
+                -- Toggle with `:Gitsigns toggle_numhl`
+                numhl = false,
+                -- Toggle with `:Gitsigns toggle_linehl`
+                linehl = false,
+                -- Toggle with `:Gitsigns toggle_word_diff`
+                word_diff = false,
+                -- Toggle with `:Gitsigns toggle_current_line_blame`
+                current_line_blame = false,
                 current_line_blame_opts = {
                     virt_text = true,
                     virt_text_pos = 'eol', -- 'eol' | 'overlay' | 'right_align'
