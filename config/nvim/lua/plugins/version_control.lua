a4e8de04cbd76394002ef6da3c9b1ff8e07f1567
Mikael Muszynski <linduxed@protonmail.com>
2025-08-08 13:13:47 +0200

    feat(neovim): default to turning on Gitsigns trailing line blame

--- config/nvim/lua/plugins/version_control.lua
+++ config/nvim/lua/plugins/version_control.lua
@@ -22,7 +22,7 @@ return {
                 -- Toggle with `:Gitsigns toggle_word_diff`
                 word_diff = false,
                 -- Toggle with `:Gitsigns toggle_current_line_blame`
-                current_line_blame = false,
+                current_line_blame = true,
                 current_line_blame_opts = {
                     virt_text = true,
                     virt_text_pos = 'eol', -- 'eol' | 'overlay' | 'right_align'
