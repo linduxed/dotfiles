61eb9eeb0b50a13d42aed4fa8c1c4865174bcc70
Mikael Muszynski <linduxed@protonmail.com>
2026-03-13 14:47:07 +0100

    style(neovim): indentation width

--- config/nvim/lua/plugins/lazyvim_overrides.lua
+++ config/nvim/lua/plugins/lazyvim_overrides.lua
@@ -1,75 +1,75 @@
 return {
-  {
-    "L3MON4D3/LuaSnip",
-    keys = {
-      {
-        "<leader>an",
-        function()
-          require("luasnip.loaders").edit_snippet_files()
+    {
+        "L3MON4D3/LuaSnip",
+        keys = {
+            {
+                "<leader>an",
+                function()
+                    require("luasnip.loaders").edit_snippet_files()
+                end,
+                desc = "Snippets - Edit",
+            },
+            {
+                "<M-.>",
+                function()
+                    local luasnip = require("luasnip")
+                    if luasnip.choice_active() then
+                        luasnip.change_choice(1)
+                    end
+                end,
+                mode = { "i", "s" },
+                desc = "Snippets - Next choice",
+            },
+            {
+                "<M-,>",
+                function()
+                    local luasnip = require("luasnip")
+                    if luasnip.choice_active() then
+                        luasnip.change_choice(-1)
+                    end
+                end,
+                mode = { "i", "s" },
+                desc = "Snippets - Prev choice",
+            },
+        },
+        opts = function(_, opts)
+            require("luasnip.loaders.from_lua").lazy_load({
+                paths = { vim.fn.stdpath("config") .. "/snippets" },
+            })
+            return opts
         end,
-        desc = "Snippets - Edit",
-      },
-      {
-        "<M-.>",
-        function()
-          local luasnip = require("luasnip")
-          if luasnip.choice_active() then
-            luasnip.change_choice(1)
-          end
-        end,
-        mode = { "i", "s" },
-        desc = "Snippets - Next choice",
-      },
-      {
-        "<M-,>",
-        function()
-          local luasnip = require("luasnip")
-          if luasnip.choice_active() then
-            luasnip.change_choice(-1)
-          end
-        end,
-        mode = { "i", "s" },
-        desc = "Snippets - Prev choice",
-      },
     },
-    opts = function(_, opts)
-      require("luasnip.loaders.from_lua").lazy_load({
-        paths = { vim.fn.stdpath("config") .. "/snippets" },
-      })
-      return opts
-    end,
-  },
-  {
-    "ThePrimeagen/harpoon",
-    keys = function()
-      local keys = {
-        {
-          "<leader>hh",
-          function()
-            require("harpoon"):list():add()
-          end,
-          desc = "Harpoon File",
-        },
-        {
-          "<leader>he",
-          function()
-            local harpoon = require("harpoon")
-            harpoon.ui:toggle_quick_menu(harpoon:list())
-          end,
-          desc = "Harpoon Quick Menu",
-        },
-      }
+    {
+        "ThePrimeagen/harpoon",
+        keys = function()
+            local keys = {
+                {
+                    "<leader>hh",
+                    function()
+                        require("harpoon"):list():add()
+                    end,
+                    desc = "Harpoon File",
+                },
+                {
+                    "<leader>he",
+                    function()
+                        local harpoon = require("harpoon")
+                        harpoon.ui:toggle_quick_menu(harpoon:list())
+                    end,
+                    desc = "Harpoon Quick Menu",
+                },
+            }
 
-      for i = 1, 9 do
-        table.insert(keys, {
-          "<leader>h" .. i,
-          function()
-            require("harpoon"):list():select(i)
-          end,
-          desc = "Harpoon to File " .. i,
-        })
-      end
-      return keys
-    end,
-  }
+            for i = 1, 9 do
+                table.insert(keys, {
+                    "<leader>h" .. i,
+                    function()
+                        require("harpoon"):list():select(i)
+                    end,
+                    desc = "Harpoon to File " .. i,
+                })
+            end
+            return keys
+        end,
+    },
 }
