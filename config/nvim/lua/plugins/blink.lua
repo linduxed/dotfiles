7eeea1df30ff5cc62baf02439e23c8a3a1c1beed
Mikael Muszynski <linduxed@protonmail.com>
2026-03-13 16:11:21 +0100

    fix(neovim): do not show ghost text for snippets with menu open

--- config/nvim/lua/plugins/blink.lua
+++ config/nvim/lua/plugins/blink.lua
@@ -2,6 +2,11 @@ return {
     {
         "saghen/blink.cmp",
         opts = {
+            completion = {
+                ghost_text = {
+                    show_with_menu = false
+                },
+            },
             sources = {
                 providers = {
                     lsp = { async = true },
