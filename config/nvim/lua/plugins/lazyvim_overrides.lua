a0ada449c08fa9e708c507e24ccbe7b209ddfe93
Mikael Muszynski <linduxed@protonmail.com>
2026-03-13 14:45:31 +0100

    feat(neovim)!: change mappings for harpoon plugin
    I was not a fan of getting my which-key menu flooded with harpoon
    mappings, so this patch moves the mappings under a namespace mapping.

--- config/nvim/lua/plugins/lazyvim_overrides.lua
+++ config/nvim/lua/plugins/lazyvim_overrides.lua
@@ -39,4 +39,37 @@ return {
       return opts
     end,
   },
+  {
+    "ThePrimeagen/harpoon",
+    keys = function()
+      local keys = {
+        {
+          "<leader>hh",
+          function()
+            require("harpoon"):list():add()
+          end,
+          desc = "Harpoon File",
+        },
+        {
+          "<leader>he",
+          function()
+            local harpoon = require("harpoon")
+            harpoon.ui:toggle_quick_menu(harpoon:list())
+          end,
+          desc = "Harpoon Quick Menu",
+        },
+      }
+
+      for i = 1, 9 do
+        table.insert(keys, {
+          "<leader>h" .. i,
+          function()
+            require("harpoon"):list():select(i)
+          end,
+          desc = "Harpoon to File " .. i,
+        })
+      end
+      return keys
+    end,
+  }
 }
