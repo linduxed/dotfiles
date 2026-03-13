61c089ad32b73e46c30e243c1f838ae6b8dcb60f
Mikael Muszynski <linduxed@protonmail.com>
2026-03-13 12:32:21 +0100

    feat(neovim): improve blink completion loading time with async
    Apparently LSP completion takes a while to load, and this blocks the
    loading of the completion menu. Setting the LSP provider to async
    significantly improves the loading time, making it almost instantaneous.

---
+++ config/nvim/lua/plugins/blink.lua
@@ -0,0 +1,12 @@
+return {
+    {
+        "saghen/blink.cmp",
+        opts = {
+            sources = {
+                providers = {
+                    lsp = { async = true },
+                },
+            },
+        },
+    },
+}
