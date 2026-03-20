4a0fe32ac80372a10e85524bfc8f72ab0889a760
Mikael Muszynski <linduxed@protonmail.com>
2026-03-20 13:46:25 +0100

    refactor(neovim): move in blink configuration into overrides file
    The `blink.lua` file was added during experimentation, and then it stuck
    after the right content was found. I don't see it as being very
    different from the contents of `lazyvim_overrides.lua`, so let's move
    the content over there.

--- config/nvim/lua/plugins/lazyvim_overrides.lua
+++ config/nvim/lua/plugins/lazyvim_overrides.lua
@@ -123,4 +123,19 @@ return {
             return keys
         end,
     },
+    {
+        "saghen/blink.cmp",
+        opts = {
+            completion = {
+                ghost_text = {
+                    show_with_menu = false
+                },
+            },
+            sources = {
+                providers = {
+                    lsp = { async = true },
+                },
+            },
+        },
+    },
 }
